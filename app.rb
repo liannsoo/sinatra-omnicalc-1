43b2a98e4da39088f9b965b1986fd2315ec97ddc
liannsoo <asoosai@uchicago.edu>
2024-10-21 17:21:07 +0000

    initial

--- app.rb
+++ app.rb
@@ -1,9 +1,53 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Square calculation
+get '/square/new' do
+  erb :square_form
+end
+
+post '/square' do
+  number = params[:number].to_i
+  @result = number ** 2
+  erb :square_result
+end
+
+# Square root calculation
+get '/square_root/new' do
+  erb :square_root_form
+end
+
+post '/square_root' do
+  number = params[:number].to_f
+  @result = Math.sqrt(number)
+  erb :square_root_result
+end
+
+# Random number generation
+get '/random/new' do
+  erb :random_form
+end
+
+post '/random' do
+  min = params[:min].to_i
+  max = params[:max].to_i
+  @result = rand(min..max)
+  erb :random_result
+end
+
+# Loan payment calculation
+get '/payment/new' do
+  erb :payment_form
+end
+
+post '/payment' do
+  apr = params[:apr].to_f / 100 / 12
+  years = params[:years].to_i * 12
+  present_value = params[:present_value].to_f
+
+  numerator = apr * present_value
+  denominator = 1 - (1 + apr) ** -years
+
+  @monthly_payment = numerator / denominator
+  erb :payment_result
 end
